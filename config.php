fb6b7afabdff4b237545ce673f6b7c5f5cf633a6
U-IkrurPC\Ikrur <Ikrur@IkrurPC.(none)>
2014-08-07 08:36:34 +0700

    oke. fix.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_H', 'localhost');
+define('DB_U', 'root');
+define('DB_P', '');
+define('DB_D', 'artikel');
+
+?>
---
+++ kePHP.class.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once './config.php';
+require_once './error_handler.php';
+
+/**
+ * Class KePHP
+ * @author Ibnu
+ */
+class KePHP
+{
+	private $mMysqli;
+	
+    public function __construct()
+    {
+        $this->mMysqli = new mysqli(DB_H, DB_U, DB_P, DB_D);
+    }
+    function __destruct()
+    {
+        $this->mMysqli->close();
+    }
+
+    public function cekKeDB($kalimat)
+    {
+        $kalimat = $this->mMysqli->real_escape_string($kalimat);
+        if ($kalimat == null) {
+            return 0;
+        }
+        $kueri = $this->mMysqli->query('select id_skrip, ju_skrip from skripsi where is_skrip like "%' . $kalimat . '%"');
+        if ($this->mMysqli->affected_rows > 0) {
+            $baris = $kueri->fetch_array(MYSQLI_ASSOC);
+            return '<tr><td>' . $kalimat . '</td><td>' . $baris['id_skrip'] . '</td><td>' . $baris['ju_skrip'] . '</td></tr>';
+        } /*else {
+            return "<tr><td></td><td>null</td><td>null</td></tr>";
+        } */
+    }
+    
+}
+

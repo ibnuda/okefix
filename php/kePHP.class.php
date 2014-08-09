e0b7fb7cf929f00da10d50cf588e7e44b03e4865
U-IkrurPC\Ikrur <Ikrur@IkrurPC.(none)>
2014-08-10 01:06:05 +0700

    nambah.

--- php/kePHP.class.php
+++ php/kePHP.class.php
@@ -29,16 +29,25 @@ class KePHP
         $kueri = $this->mMysqli->query('select id_skrip, ju_skrip from skripsi where is_skrip like "%' . $kalimat . '%"');
         if ($this->mMysqli->affected_rows > 0) {
             $baris = $kueri->fetch_array(MYSQLI_ASSOC);
+            /*
             if (strlen($kalimat) >= 60){
                 $kalimat = substr($kalimat, 0, 60);
                 $kalimat = $kalimat . '...';
             }
+            */
             return '<tr class="warning"><td>' . $kalimat . '</td><td>' . $baris['id_skrip'] . '</td><td>' . $baris['ju_skrip'] . '</td></tr>';
             //return '<ul><li>' . $kalimat . '</li><li>' . $baris['id_skrip'] . '</li><li>' . $baris['ju_skrip'] . '</li></ul>';
-        } /*else {
-            return "<tr><td></td><td>null</td><td>null</td></tr>";
-        } */
+        } else {
+            return "<tr class='warning'><td>" . $kalimat . "</td><td>null</td><td>null</td></tr>";
+        } 
     }
+	public function ngeprin($a, $b){
+		$kek = $this->mMysqli->query('select id_skrip, ju_skrip from skripsi id_skrip between ' . $a . ' and ' . $b .';');
+		$heem = $kek->fetch_array();
+		return $heem['id_skrip'] . $heem['ju_skrip'];
+	}
+    
+
     
 }
 
